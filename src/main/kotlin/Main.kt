4bb3ff68f797cfd82a7be48b35f33fc47cd165ff
erastusnzula <nzulaerastus@gmail.com>
2022-05-11 17:28:02 +0300

    Changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,42 +1,42 @@
-fun main(args: Array<String>) {
-    val choices = arrayOf("Paper", "Rock", "Scissor")
+fun main(args:Array<String>){
+    val choices = arrayOf("Paper", "Scissor","Rock")
     val gameChoice = getGameChoice(choices)
-    val userInputChoice = getUserInputChoice(choices)
-    showResults(userInputChoice, gameChoice)
+    val userChoice = getUserChoice(choices)
+    showResults(gameChoice, userChoice)
+}
+
+fun getGameChoice(choice: Array<String>): String{
+    val randomIndex = (Math.random() * choice.size).toInt()
+    return choice[randomIndex]
 }
 
 
-fun getGameChoice(choices: Array<String>): String {
-    val randomIndex = (Math.random() * choices.size).toInt()
-    return choices[randomIndex]
-}
-
-fun getUserInputChoice(choices: Array<String>): String {
-    var choice = ""
+fun getUserChoice(choices: Array<String>) : String{
+    var userChoice = ""
     var valid = false
 
-    while (!valid) {
-        println("Please enter one of the following choices: ")
-        for ((i, item) in choices.withIndex()) {
-            println(" $i : $item")
+    while (!valid){
+        println("Enter one of the following choices : ")
+        for ((i,choice) in choices.withIndex()){
+            println("$i : $choice")
         }
         val userInput = readLine()
-        if (userInput != null && userInput in choices) {
+        if (userInput != null && userInput in choices){
             valid = true
-            choice = userInput
-        } else {
-            println("Enter a valid choice")
+            userChoice = userInput
+        }
+        else{
+            println("Enter a valid choice.")
         }
     }
-    return choice
+    return userChoice
 }
 
-fun showResults(userInput: String, gameChoice: String) {
-    val results: String = if (userInput == gameChoice) "Tie"
-    else if ((userInput == "Paper" && gameChoice == "Rock") ||
-        (userInput == "Scissor" && gameChoice == "Paper") ||
-        (userInput == "Rock" && gameChoice == "Scissor")
-    ) "You win"
-    else "You lose"
-    println("Your choice $userInput, I chose $gameChoice. $results")
+fun showResults(gameChoice: String, userChoice: String){
+    val results : String = if(userChoice==gameChoice) "Tie."
+    else if ((userChoice=="Paper" && gameChoice=="Rock")||
+        (userChoice=="Rock" && gameChoice=="Scissor")||
+        (userChoice=="Scissor" && gameChoice=="Paper")) "You win."
+    else "You lose."
+    println("Your choice : $userChoice, my choice : $gameChoice, $results")
 }
